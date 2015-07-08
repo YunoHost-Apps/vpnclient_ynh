f43242301996f8c8ced2431a42be23cb09882720
Julien VAUBOURG <julien@vaubourg.com>
2015-07-08 19:46:58 +0200

    Fix wrong header

--- sources/config.php
+++ sources/config.php
@@ -1,8 +1,8 @@
 <?php
 
-/* Wifi Hotspot app for YunoHost 
+/* VPN Client app for YunoHost 
  * Copyright (C) 2015 Julien Vaubourg <julien@vaubourg.com>
- * Contribute at https://github.com/jvaubourg/hotspot_ynh
+ * Contribute at https://github.com/jvaubourg/vpnclient_ynh
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
